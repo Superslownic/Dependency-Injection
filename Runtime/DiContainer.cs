97b5177cfa3310b8615fb13f962b3a6d70adcab4
Daniil <isuperslownic@gmail.com>
2022-08-29 12:16:36 +0300

    Added asmdef

--- Runtime/DiContainer.cs
+++ Runtime/DiContainer.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Reflection.Baking;
-using UnityEngine;
-using Object = System.Object;
+using ReflectionBaking;
 
 namespace DependencyInjection
 {
